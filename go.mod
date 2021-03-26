d98697005c10a31522f24b7767425ca0191e14bf
Paulo <pgsoares@telecomboutique.com>
2021-03-26 15:33:37 +0100

    ok

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/Kucoin/kucoin-go-sdk v1.2.8
 	github.com/fatih/color v1.10.0
+	github.com/gookit/color v1.3.8 // indirect
 )
--- main.go
+++ main.go
@@ -7,12 +7,21 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"os"
 	"strconv"
-
 	"strings"
+	"time"
 )
 
 func main() {
+	colorReset := "\033[0m"
+
+	colorRed := "\033[31m"
+	colorGreen := "\033[32m"
+	colorCyan := "\033[36m"
+
+	// colorYellow := "\033[33m"
+
 	initialPrice := 0.0
 	initialBuyPrice := 0.0
 	highPrice := 0.0
@@ -21,9 +30,10 @@ func main() {
 	selectedPairBalance := 0.0
 	sellQuantity := 0.0
 	buyQuantity := 0.0
+	lastPrice := 0.0
 	orderId := ""
 	coinExist := false
-	coinName := "go"
+	coinName := "ada"
 	pairCoinName := "usdt"
 	selectedCoin := strings.ToUpper(coinName)
 	selectedPair := strings.ToUpper(pairCoinName)
@@ -60,17 +70,16 @@ func main() {
 	}
 	//-------------------------------------------------------
 
-        fmt.Print("Enter text: ")
-        var input string
-        fmt.Scanln(&input)
-        selectedCoin = strings.ToUpper(input)               
-        selectedPair = strings.ToUpper(pairCoinName)
-        selectedSymbol = selectedCoin + "-" + selectedPair
-
+	//fmt.Print("Enter text: ")
+	//var input string
+	//fmt.Scanln(&input)
+	//selectedCoin = strings.ToUpper(input)
+	//selectedPair = strings.ToUpper(pairCoinName)
+	//selectedSymbol = selectedCoin + "-" + selectedPair
 
 	selectedSymbolBalance = parsePriceToFloat(getBalanceByCoin(kucoinService, coinName))
 	selectedPairBalance = parsePriceToFloat(getBalanceByCoin(kucoinService, pairCoinName))
-	sellQuantity = math.Round(selectedSymbolBalance - (selectedSymbolBalance * 2 / 100))
+	sellQuantity = math.Round(selectedSymbolBalance - (selectedSymbolBalance * 3 / 100))
 
 	fmt.Println("selectedSymbolBalance")
 	fmt.Println(selectedSymbolBalance)
@@ -90,7 +99,7 @@ func main() {
 
 			// todo cambiar por precio compra initialBuyPrice
 			// todo comprar
-			buyQuantity = math.Round((selectedPairBalance / initialPrice) - ((selectedPairBalance / initialPrice) * 2 / 100))
+			buyQuantity = math.Round((selectedPairBalance / initialPrice) - ((selectedPairBalance / initialPrice) * 3 / 100))
 			orderResult := createMarketOrder(kucoinService, "buy", selectedSymbol, parsePriceToString(buyQuantity))
 			orderId = orderResult.OrderId
 			fmt.Println(orderId)
@@ -100,7 +109,12 @@ func main() {
 				tickerAfterBuy := getSymbolTicker(kucoinService, selectedSymbol)
 				initialBuyPrice = parsePriceToFloat(tickerAfterBuy.BestAsk)
 				selectedSymbolBalance = parsePriceToFloat(getBalanceByCoin(kucoinService, coinName))
-				sellQuantity = math.Round(selectedSymbolBalance - (selectedSymbolBalance * 2 / 100))
+				sellQuantity = math.Round(selectedSymbolBalance - (selectedSymbolBalance * 3 / 100))
+				stopLossPrice = initialBuyPrice - (initialBuyPrice * 5 / 100)
+
+				fmt.Println(sellQuantity)
+				fmt.Println(stopLossPrice)
+				os.Exit(0)
 
 			}
 
@@ -130,13 +144,13 @@ func main() {
 				return
 			}
 			// ---------------------------------------------------------------------------------------------------------
-			fmt.Printf("Ticker: %s, %s\n", ticker.BestBid, ticker.BestAsk)
-
 			// new target
 			currentPrice := parsePriceToFloat(ticker.BestBid)
 			if currentPrice > initialBuyPrice && currentPrice > highPrice {
 				highPrice = parsePriceToFloat(ticker.BestBid)
 				color.Yellow("Nuevo precio más alto")
+
+				/*
 				stopPrice := highPrice - (highPrice * 5 / 100)
 				sellPrice := highPrice - (highPrice * 10 / 100)
 
@@ -144,7 +158,6 @@ func main() {
 				fmt.Println(sellPrice)
 				fmt.Println("stopPrice")
 				fmt.Println(stopPrice)
-
 				cancelOrder(kucoinService, orderId)
 				orderResult := createTakeProfitOrder(
 					kucoinService,
@@ -154,16 +167,49 @@ func main() {
 					parsePriceToString(sellPrice))
 
 				orderId = orderResult.OrderId
+			*/
+			}
+
+			if currentPrice <= highPrice-(highPrice*5/100) {
+				cancelOrder(kucoinService, orderId)
+				orderResult := createMarketOrder(kucoinService, "sell", selectedSymbol, parsePriceToString(sellQuantity))
+				orderId = orderResult.OrderId
+
+				color.Red("TAKE PROFIT")
+				color.Red("currentPrice")
+				color.Red(parsePriceToString(currentPrice))
+
+				color.Red("limit")
+				color.Red(parsePriceToString(highPrice - (highPrice * 5 / 100)))
 			}
 
 			// STOP LOSS SELL
 			if currentPrice <= stopLossPrice {
 				color.Red("STOP LOSS")
+				color.Red("currentPrice")
+				color.Red(parsePriceToString(currentPrice))
+
+				color.Red("stopLossPrice")
+				color.Red(parsePriceToString(stopLossPrice))
+
 				cancelOrder(kucoinService, orderId)
 				orderResult := createMarketOrder(kucoinService, "sell", selectedSymbol, parsePriceToString(sellQuantity))
 				orderId = orderResult.OrderId
 				return
 			}
+
+			priceColor := colorRed
+
+			if lastPrice > highPrice {
+				priceColor = colorGreen
+			} else if lastPrice < currentPrice {
+				priceColor = colorRed
+			} else {
+				priceColor = colorCyan
+			}
+
+			fmt.Printf("\033[2K\r"+priceColor+"%s "+colorGreen+"%s "+colorRed+"%s "+colorReset, parsePriceToString(currentPrice), ticker.BestBid, ticker.BestAsk)
+			lastPrice = currentPrice
 
 			// ---------------------------------------------------------------------------------------------------------
 			if err = c.Subscribe(ch1); err != nil {
@@ -174,6 +220,18 @@ func main() {
 		}
 	}
 
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
 }
 
 func getBalanceByCoin(kucoinService *kucoin.ApiService, currency string) string {
@@ -202,11 +260,12 @@ func parsePriceToFloat(price string) float64 {
 	return f
 }
 func parsePriceToString(price float64) string {
-	s := fmt.Sprintf("%.5f", price)
+	s := fmt.Sprintf("%.4f", price)
 	return s
 }
 
 func createMarketOrder(kucoinService *kucoin.ApiService, side, symbol, size string) *kucoin.CreateOrderResultModel {
+	rand.Seed(time.Now().UnixNano())
 	oid := strconv.FormatInt(int64(rand.Intn(99999999)), 10)
 
 	order := &kucoin.CreateOrderModel{
@@ -264,34 +323,6 @@ func createTakeProfitOrder(kucoinService *kucoin.ApiService, symbol, size, stopP
 	return createOrderResultModel
 }
 
-func createBuyOrder(kucoinService *kucoin.ApiService, symbol string, size string, price string) *kucoin.CreateOrderResultModel {
-	createOrderResultModel := &kucoin.CreateOrderResultModel{}
-	oid := strconv.FormatInt(int64(rand.Intn(99999999)), 10)
-
-	order := &kucoin.CreateOrderModel{
-		ClientOid: oid,
-		Side:      "buy",
-		Symbol:    symbol,
-		Price:     price,
-		Size:      size,
-	}
-
-	createOrderResult, err := kucoinService.CreateOrder(order)
-	if err != nil {
-		// fmt.Println(err.Error())
-		return nil
-	}
-
-	err = createOrderResult.ReadData(createOrderResultModel)
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil
-	}
-
-	fmt.Println(createOrderResultModel)
-	return createOrderResultModel
-}
-
 func getOrder(kucoinService *kucoin.ApiService, orderId string) *kucoin.OrderModel {
 	order := &kucoin.OrderModel{}
 	getOrderResult, err := kucoinService.Order(orderId)
@@ -311,19 +342,18 @@ func cancelOrder(kucoinService *kucoin.ApiService, orderId string) bool {
 	cancelOrderModel := &kucoin.CancelOrderResultModel{}
 
 	order := getOrder(kucoinService, orderId)
-	if order.IsActive {
-		cancelOrderResult, err := kucoinService.CancelOrder(orderId)
-		if err != nil {
-			fmt.Println(err.Error())
-			return false
-		}
-		err = cancelOrderResult.ReadData(cancelOrderModel)
-		if err != nil {
-			fmt.Println(err.Error())
-			return false
-		}
-	}
+	fmt.Println(order)
 
+	cancelOrderResult, err := kucoinService.CancelOrder(orderId)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	err = cancelOrderResult.ReadData(cancelOrderModel)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
 
 	return true
 }
